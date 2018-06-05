6ff5a015c0ae9d02b90ac3af04e3809d5139c673
dtimokhi <dstimokhin@gmail.com>
2018-06-04 21:30:44 -0700

    dimas code

--- EvaluateCFList.py
+++ EvaluateCFList.py
@@ -3,6 +3,55 @@ import pandas as pd
 import EvaluateCFRandom as helper
 import sys
 
+class JokerObject:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.item_means = np.nanmean(matrix, 0)
+        self.user_means = np.nanmean(matrix, 1)
+    def get_matrix(self):
+        return self.matrix
+    def get_items_m(self):
+        return self.item_means
+    def get_item_m(self, key):
+        return self.item_means[key]
+    def get_users_m(self):
+        return self.user_means
+    def get_user_m(self, key):
+        return self.user_means[key]
+    def get_item(self, row, col):
+        return self.matrix[row][col]
+    def set_object_nan(self, row, col):
+        self.matrix[row][col] = np.nan
+    def get_pearson_matrix(self):
+        distances = np.zeros((len(self.user_means),len(self.user_means)))
+        for i, user_1 in enumerate(self.matrix):
+            for j, user_2 in enumerate(self.matrix):
+                if(i>j):
+                    continue
+                if i != j:
+                    distances[i,j] = np.sum((a-self.user_means[i])*(b-self.user_means[j]) for a,b in zip(user_1,user_2) if np.isnan(a)== False and np.isnan(b) == False)/np.sqrt(np.sum(np.square(a-self.user_means[i]) for index, a in enumerate(user_1) if np.isnan(a)== False and np.isnan(user_2[index]) == False)*np.sum(np.square(b-self.user_means[j]) for index, b in enumerate(user_2) if np.isnan(b)== False and np.isnan(user_1[index]) == False))
+                    distances[j,i] = distances[i,j]
+        return distances
+                    
+def get_avg_weighted_sum(distance_matrix, data_matrix, coords):
+    if not np.isnan(data_matrix.get_item(coords[0],coords[1])):
+        data_matrix = copy.deepcopy(data_matrix)
+        data_matrix.set_object_nan(coords[0],coords[1])
+    return data_matrix.get_user_m(coords[0]) + (1/distance_matrix.sum(axis=0)[coords[0]])*np.sum(distance_matrix[coords[0]][c_prime]*(data_matrix.get_item(c_prime, coords[1])-data_matrix.get_user_m(c_prime))for c_prime in range(distance_matrix.shape[0]) if not np.isnan(data_matrix.get_item(c_prime, coords[1])))
+
+def get_average_ranking(data_matrix, coords):
+    if not np.isnan(data_matrix.get_item(coords[0],coords[1])):
+        data_matrix = copy.deepcopy(data_matrix)
+        data_matrix.set_object_nan(coords[0],coords[1])
+    utilities = [data_matrix.get_item(c,coords[1])for c in range(data_matrix.get_matrix().shape[0]) if not np.isnan(data_matrix.get_item(c, coords[1]))]
+    return (1/len(utilities))*sum(utilities)
+
+def get_adjusted_weighted_Nnn_sum(distance_matrix, data_matrix, coords, N = 3):
+    if not np.isnan(data_matrix.get_item(coords[0],coords[1])):
+        data_matrix = copy.deepcopy(data_matrix)
+        data_matrix.set_object_nan(coords[0],coords[1])
+    return data_matrix.get_user_m(coords[0]) + (1/distance_matrix.sum(axis=0)[coords[0]])*np.sum(distance_matrix[coords[0]][c_prime]*(data_matrix.get_item(c_prime, coords[1])-data_matrix.get_user_m(c_prime))for c_prime in dist[coords[0],].argsort()[:N] if not np.isnan(data_matrix.get_item(c_prime, coords[1])))
+
 def get_matrix():
     return pd.read_csv('jester-data-1.csv', header=None, na_values=99).iloc[:,1:].values
 
